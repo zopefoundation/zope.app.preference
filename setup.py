02c2539ec429a121be864c420b8afa42dfc561aa
Dan Korostelev <nadako@gmail.com>
2009-01-17 13:36:57 +0000

    Tag 3.5.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*rnames):
 setup(name = 'zope.app.preference',
       version = '3.5.0',
       author='Zope Corporation and Contributors',
-      author_email='zope3-dev@zope.org',
+      author_email='zope-dev@zope.org',
       description='User Preferences Framework',
       long_description=(
           read('README.txt')
@@ -47,7 +47,7 @@ setup(name = 'zope.app.preference',
           'Operating System :: OS Independent',
           'Topic :: Internet :: WWW/HTTP',
           'Framework :: Zope3'],
-      url='http://cheeseshop.python.org/pypi/zope.app.preference',
+      url='http://pypi.python.org/pypi/zope.app.preference',
       license='ZPL 2.1',
       packages=find_packages('src'),
       package_dir = {'': 'src'},
