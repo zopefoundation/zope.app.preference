115d375acd20c4db9e6e75d96ea988701757dd8d
Michael Howitz <mh@gocept.com>
2010-06-11 17:23:32 +0000

    Preparing release 3.7.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name = 'zope.app.preference',
-      version = '3.6.1dev',
+      version='3.7.0',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='User Preferences Framework',
