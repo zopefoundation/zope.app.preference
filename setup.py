f18e0009ca5d886ef445353469d10eebc3538999
Jason Madden <jamadden@gmail.com>
2017-05-17 06:24:07 -0500

    Back to development: 4.0.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(*rnames):
         return f.read()
 
 setup(name='zope.app.preference',
-      version='4.0.0',
+      version='4.0.1.dev0',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='User Preferences Framework ZMI UI',
